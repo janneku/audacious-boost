2cd697946e4040e4da528783e5f8b71d93bf0c6b
Janne Kulmala <janne.t.kulmala@tut.fi>
2012-03-10 12:35:36 +0200

    gui: Reindent to Linux kernel coding style [CLEANUP]

--- gui.c
+++ gui.c
@@ -10,9 +10,9 @@
 #include "boost.h"
 
 static const char *boost_about_text =
-"Boost Plugin\n"
-"By Johan Levin 1999.\n\n"
-"Surround boost by Carl van Schaik 1999";
+    "Subwoofer simulation plugin\n"
+    "By Janne Kulmala 2012\n"
+    "Based on XMMS/Audacious echo plugin by Johan Levin 1999.";
 
 static GtkWidget *conf_dialog = NULL;
 static GtkAdjustment *boost_delay_adj, *boost_feedback_adj, *boost_volume_adj;
@@ -20,10 +20,10 @@ static GtkAdjustment *boost_cutoff_adj;
 
 void boost_about(void)
 {
-	static GtkWidget * boost_about_dialog = NULL;
+	static GtkWidget *boost_about_dialog = NULL;
 
-	audgui_simple_message(& boost_about_dialog, GTK_MESSAGE_INFO,
-	 "About Boost Plugin", boost_about_text);
+	audgui_simple_message(&boost_about_dialog, GTK_MESSAGE_INFO,
+			      "About Boost Plugin", boost_about_text);
 }
 
 static void apply_changes(void)
@@ -35,7 +35,7 @@ static void apply_changes(void)
 	boost_cutoff = gtk_adjustment_get_value(boost_cutoff_adj);
 
 	cfg = aud_cfg_db_open();
-	if (! cfg)
+	if (!cfg)
 		return;
 	aud_cfg_db_set_int(cfg, "boost_plugin", "delay", boost_delay);
 	aud_cfg_db_set_int(cfg, "boost_plugin", "feedback", boost_feedback);
@@ -68,44 +68,54 @@ void boost_configure(void)
 		return;
 
 	conf_dialog = gtk_dialog_new();
-	g_signal_connect(conf_dialog, "destroy", (GCallback) gtk_widget_destroyed,
-	 & conf_dialog);
+	g_signal_connect(conf_dialog, "destroy",
+			 (GCallback) gtk_widget_destroyed, &conf_dialog);
 	gtk_window_set_title(GTK_WINDOW(conf_dialog), "Configure Boost");
 
 	boost_delay_adj = (GtkAdjustment *) gtk_adjustment_new(boost_delay, 0,
-	 MAX_DELAY + 10, 2, 10, 10);
-	boost_feedback_adj = (GtkAdjustment *) gtk_adjustment_new(boost_feedback, 0,
-	 100 + 10, 2, 10, 10);
-	boost_volume_adj = (GtkAdjustment *) gtk_adjustment_new(boost_volume, 0, 100
-	 + 10, 2, 10, 10);
-	boost_cutoff_adj = (GtkAdjustment *) gtk_adjustment_new(boost_cutoff,
-         MIN_CUTOFF, MIN_CUTOFF * 10 + 10, 2, 10, 10);
+							       MAX_DELAY + 10,
+							       2, 10, 10);
+	boost_feedback_adj =
+	    (GtkAdjustment *) gtk_adjustment_new(boost_feedback, 0, 100 + 10, 2,
+						 10, 10);
+	boost_volume_adj =
+	    (GtkAdjustment *) gtk_adjustment_new(boost_volume, 0, 100 + 10, 2,
+						 10, 10);
+	boost_cutoff_adj =
+	    (GtkAdjustment *) gtk_adjustment_new(boost_cutoff, MIN_CUTOFF,
+						 MIN_CUTOFF * 10 + 10, 2, 10,
+						 10);
 
 	table = gtk_table_new(2, 4, FALSE);
 	gtk_table_set_col_spacings(GTK_TABLE(table), 5);
 	gtk_container_set_border_width(GTK_CONTAINER(table), 5);
 	gtk_box_pack_start((GtkBox *) gtk_dialog_get_content_area((GtkDialog *)
-	 conf_dialog), table, TRUE, TRUE, 5);
+								  conf_dialog),
+			   table, TRUE, TRUE, 5);
 	gtk_widget_show(table);
 
 	label = gtk_label_new("Delay: (ms)");
 	gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
-	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1, GTK_FILL, GTK_FILL, 0, 0);
+	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1, GTK_FILL,
+			 GTK_FILL, 0, 0);
 	gtk_widget_show(label);
 
 	label = gtk_label_new("Feedback: (%)");
 	gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
-	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 1, 2, GTK_FILL, GTK_FILL, 0, 0);
+	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 1, 2, GTK_FILL,
+			 GTK_FILL, 0, 0);
 	gtk_widget_show(label);
 
 	label = gtk_label_new("Volume: (%)");
 	gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
-	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 2, 3, GTK_FILL, GTK_FILL, 0, 0);
+	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 2, 3, GTK_FILL,
+			 GTK_FILL, 0, 0);
 	gtk_widget_show(label);
 
 	label = gtk_label_new("Cutoff: (Hz)");
 	gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
-	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 3, 4, GTK_FILL, GTK_FILL, 0, 0);
+	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 3, 4, GTK_FILL,
+			 GTK_FILL, 0, 0);
 	gtk_widget_show(label);
 
 	hscale = gtk_hscale_new(GTK_ADJUSTMENT(boost_delay_adj));
@@ -131,7 +141,8 @@ void boost_configure(void)
 	bbox = gtk_hbutton_box_new();
 	gtk_button_box_set_layout(GTK_BUTTON_BOX(bbox), GTK_BUTTONBOX_END);
 	gtk_box_pack_start((GtkBox *) gtk_dialog_get_action_area((GtkDialog *)
-	 conf_dialog), bbox, TRUE, TRUE, 0);
+								 conf_dialog),
+			   bbox, TRUE, TRUE, 0);
 
 	button = gtk_button_new_with_label("Ok");
 	gtk_widget_set_can_default(button, TRUE);
